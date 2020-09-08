0bcdc04e859d4b7bf4d8cfd0c35abe890046eefb
silas-gross <silas.grossberndt@gmail.com>
2020-09-08 15:55:08 -0400

    this is good enough for now

--- shape.py
+++ shape.py
@@ -9,6 +9,7 @@ top=tk.Tk()
 p2=[40,430]
 p3=[460, 430]
 p1=[250,70]
+tracker="g=1"
 w=tk.Canvas(top,  cursor="dot", height=500, width="500")
 
 def triangle(px1, px2, px3):
@@ -20,44 +21,40 @@ def triangle(px1, px2, px3):
     dividing_line=w.create_line(250,83, 250,450, dash=(10,))
     w.pack()
 def text(px1, px2):
+    global tracker
     #tex=tk.Canvas(top, height=100, width=500)
     if px1==[250, 70] and px2==[40,430]:
         w.create_text(250, 470, text="g=1")
     if px1==[250, 70] and px2==[460,430]:
         w.create_text(250, 470, text="g=s")
     if px1==[460, 430] and px2==[40,430]:
-        w.create_text( 250,470, text="g=rs")
+        w.create_text( 250,470, text="g=r*r*s")
     if px1==[40, 430] and px2==[460,430]:
-        w.create_text(250, 470, text="g=r")
+        w.create_text(250, 470, text="g=r*r")
     if px1==[460, 430] and px2==[250,70]:
-        w.create_text(250,470, text="g=r^2")
+        w.create_text(250,470, text="g=r")
     if px1==[40, 430] and px2==[250,70]:
-        w.create_text(250,470, text="g=r^2s")
-def rf(px1, px2, px3):
+        w.create_text(250,470, text="g=r*s")
+    w.create_text(250, 485, text=tracker)
+def rf():
+    global p1, p2, p3, tracker
     w.delete('all')
-    p1=px1
-    p3=px2
-    p2=px3
+    p1, p2, p3= p1, p3, p2
     triangle(p1, p2, p3)
+    #text(p1, p2)
+    tracker=tracker+"*s"
     text(p1, p2)
-def ro(px1, px2, px3):
+def ro():
+    global p1, p2, p3, tracker
     w.delete('all')
-    #print(p1)
-    p1=px3
-    print("p1 is now:" +str(px1))
-    p2=px1
-    p3=px2
+    p1, p2, p3= p2, p3, p1
     triangle(p1, p2, p3)
+    tracker=tracker+"*r"
     text(p1, p2)
 def buttons():
-    #p2=[40,430]
-    #p3=[460, 430]
-    #p1=[250,70]
-    print(p1)
-    l=tk.Button(top, text="Rotate", command=lambda: ro(p1, p2, p3))
-    k=tk.Button(top, text="Reflect", command=lambda: rf(p1, p2, p3))
+    l=tk.Button(top, text="Rotate", command=ro)
+    k=tk.Button(top, text="Reflect", command=rf)
     triangle(p1, p2, p3)
-    #text(p1, p2
     k.pack()
     l.pack()
 buttons()
