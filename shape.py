4618afce54846372799a38d0eb21592169ce68aa
silas-gross <silas.grossberndt@gmail.com>
2020-09-08 12:05:17 -0400

    This is super broken right now. gotta work on it

--- shape.py
+++ shape.py
@@ -17,6 +17,21 @@ def triangle(p1, p2, p3):
 p2=[40,430]
 p3=[460, 430]
 p1=[250,70]
+def text(p1, p2):
+    tex=tk.Canvas(top, height=100, width=500)
+    if p1==[250, 70] and p2==[40,430]:
+        tex.create_text(50, 250, text="g=1")
+    if p1==[250, 70] and p2==[460,430]:
+        tex.create_text(50, 250, text="g=s")
+    if p1==[460, 430] and p2==[40,430]:
+        tex.create_text(50, 250, text="g=rs")
+    if p1==[40, 430] and p2==[460,430]:
+        tex.create_text(50, 250, text="g=r")
+    if p1==[460, 430] and p2==[250,70]:
+        tex.create_text(50, 250, text="g=r^2")
+    if p1==[40, 430] and p2==[250,70]:
+        tex.create_text(50, 250, text="g=r^2s")
+    tex.pack()
 def rf(p1, p2, p3):
     p4=p3
     p3=p2
@@ -26,13 +41,13 @@ def ro(p1, p2, p3):
     p1=p2
     p2=p3
     p3=p4
+    print(p4)
 def buttons(p1, p2, p3):
     l=tk.Button(top, text="Rotate", command=ro(p1, p2, p3))
     k=tk.Button(top, text="Reflect", command=rf(p1, p2, p3))
+    triangle(p1, p2, p3)
+    text(p1, p2)
     k.pack()
     l.pack()
-    triangle(p1, p2, p3)
-    print(p1)
 buttons(p1, p2, p3)
-
 top.mainloop()
